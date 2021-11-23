0fad8fc3161d82a1c11c72dc11d055e7e1c2c7d3
Ed <edward2.neal@live.uwe.ac.uk>
2021-11-23 16:47:26 +0000

    Added CameraController

---
+++ Assets/CameraController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float scroll= 8;
+    public Camera m_OrthographicCamera;
+
+    public float minFOV;
+    public float maxFOV;
+    public float sensitivity;
+    public float FOV;
+
+    public int scrollSpeed = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+
+        FOV = Camera.main.fieldOfView;
+        FOV += (Input.GetAxis("Mouse ScrollWheel") * sensitivity) * -1;
+        FOV = Mathf.Clamp(FOV, minFOV, maxFOV);
+        Camera.main.fieldOfView = FOV;
+
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+
+
+        transform.position = pos;
+    }
+}
